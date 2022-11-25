f3a39944c5a8df1903415b03abf2a167af02b521
michael <fuck@qq.com>
2022-11-25 17:57:00 +0800

    test

---
+++ app/src/main/java/com/example/michael/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.michael;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    MyAdapter mMyAdapter ;
+    List<News> mNewsList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // 构造一些数据
+        for (int i = 0; i < 10; i++) {
+            News news = new News();
+            news.title = "标题" + i;
+            news.content = "内容" + i;
+            news.time = "2022/11/25";
+            news.Icon = R.drawable.fuck;
+//            news.Icon = R.id.imageView;
+            mNewsList.add(news);
+        }
+        mMyAdapter = new MyAdapter();
+        mRecyclerView.setAdapter(mMyAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHoder> {
+
+        @NonNull
+        @Override
+        public MyViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = View.inflate(MainActivity.this, R.layout.item_list, null);
+            MyViewHoder myViewHoder = new MyViewHoder(view);
+            return myViewHoder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHoder holder, int position) {
+            News news = mNewsList.get(position);
+            holder.mTitleTv.setText(news.title);
+            holder.mTitleContent.setText(news.content);
+            holder.time.setText(news.time);
+//            holder.smallImg.setImageAlpha(news.Icon);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
+    }
+    class MyViewHoder extends RecyclerView.ViewHolder {
+        TextView mTitleTv;
+        TextView mTitleContent;
+        TextView time;
+        ImageView smallImg;
+        public MyViewHoder(@NonNull View itemView) {
+            super(itemView);
+            mTitleTv = itemView.findViewById(R.id.textView);
+            mTitleContent = itemView.findViewById(R.id.textView2);
+            time = itemView.findViewById(R.id.textView3);
+            smallImg = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/michael/News.java
@@ -0,0 +1,8 @@
+package com.example.michael;
+
+public class News {
+    public String title;
+    public String content;
+    public String time;
+    public int Icon;
+}
