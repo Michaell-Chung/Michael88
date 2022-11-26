a697e2de5cae99362fb8bbfa44080aae0358ce26
michael <fuck@qq.com>
2022-11-26 20:25:37 +0800

    长按contextmenu

--- app/src/main/java/com/example/michael/MainActivity.java
+++ app/src/main/java/com/example/michael/MainActivity.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,18 +26,55 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     MyAdapter mMyAdapter ;
     List<News> mNewsList = new ArrayList<>();
+    List<Integer> drawImg = new ArrayList<>();
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    public void CreateMenu(Menu menu)
+    {
+        int groupID = 0;
+        int order = 0;
+        int[] itemID = {1,2};
+
+        for(int i=0;i<itemID.length;i++)
+        {
+            switch(itemID[i])
+            {
+                case 1:
+                    menu.add(groupID, itemID[i], order, "义乌");
+                    break;
+                case 2:
+                    menu.add(groupID, itemID[i], order, "芝士雪豹");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.recyclerview);
+        drawImg.add(R.drawable.fuck);
+        drawImg.add(R.drawable.desert);
         // 构造一些数据
         for (int i = 0; i < 10; i++) {
             News news = new News();
             news.title = "标题" + i;
             news.content = "内容" + i;
             news.time = "2022/11/25";
-            news.Icon = R.drawable.fuck;
+            news.Icon = drawImg.get(i%2);
 //            news.Icon = R.id.imageView;
             mNewsList.add(news);
         }
@@ -39,17 +82,25 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mMyAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(layoutManager);
-
     }
-    class MyAdapter extends RecyclerView.Adapter<MyViewHoder> {
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHoder> {
+        private Context mContext;
+        private int position;
+        public int getContextMenuPosition() { return position; }
+        public void setContextMenuPosition(int position) { this.position = position; }
+
 
         @NonNull
         @Override
         public MyViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if(mContext==null){
+                mContext = parent.getContext();
+            }
             View view = View.inflate(MainActivity.this, R.layout.item_list, null);
             MyViewHoder myViewHoder = new MyViewHoder(view);
             return myViewHoder;
         }
+
 
         @Override
         public void onBindViewHolder(@NonNull MyViewHoder holder, int position) {
@@ -57,26 +108,41 @@ public class MainActivity extends AppCompatActivity {
             holder.mTitleTv.setText(news.title);
             holder.mTitleContent.setText(news.content);
             holder.time.setText(news.time);
-//            holder.smallImg.setImageAlpha(news.Icon);
+            holder.smallImg.setImageResource(news.Icon);
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    setContextMenuPosition(holder.getLayoutPosition());
+                    return false;
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
             return mNewsList.size();
         }
-    }
-    class MyViewHoder extends RecyclerView.ViewHolder {
-        TextView mTitleTv;
-        TextView mTitleContent;
-        TextView time;
-        ImageView smallImg;
-        public MyViewHoder(@NonNull View itemView) {
-            super(itemView);
-            mTitleTv = itemView.findViewById(R.id.textView);
-            mTitleContent = itemView.findViewById(R.id.textView2);
-            time = itemView.findViewById(R.id.textView3);
-            smallImg = itemView.findViewById(R.id.imageView);
+        class MyViewHoder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
+            TextView mTitleTv;
+            TextView mTitleContent;
+            TextView time;
+            ImageView smallImg;
+            public MyViewHoder(@NonNull View itemView) {
+                super(itemView);
+                mTitleTv = itemView.findViewById(R.id.textView);
+                mTitleContent = itemView.findViewById(R.id.textView2);
+                time = itemView.findViewById(R.id.textView3);
+                smallImg = itemView.findViewById(R.id.imageView);
+                itemView.setOnCreateContextMenuListener(this);
+            }
+
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                News mSelectModelUser = mNewsList.get(getContextMenuPosition());
+                Log.i("UserAdapter", "onCreateContextMenu: "+getContextMenuPosition());
+                contextMenu.setHeaderTitle(mSelectModelUser.title);
+                ((MainActivity)mContext).CreateMenu(contextMenu);
+            }
         }
     }
-
 }
